86f6167f8f7581cfb1b27d2d376dc354c020dc91
Maksim Kuntsevich <max-cun4@yandex.ru>
2024-12-06 21:15:42 +0300

    Add day 5 part 1 solution

---
+++ day5/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import "testing"
+
+var example = `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+
+func Test_part1(t *testing.T) {
+	want := 143
+	if got := part1(example); got != want {
+		t.Errorf("part1() = %v; want %v", got, want)
+	}
+}
+
+var example2 string
+
+func Test_part2(t *testing.T) {
+	want := 0
+	if got := part2(example2); got != want {
+		t.Errorf("part2() = %v; want %v", got, want)
+	}
+}
